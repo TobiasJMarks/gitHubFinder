916cd8b011b076165db477ac99e93a5ab9d780a1
TobiasJMarks <toby.marks@live.com>
2020-10-27 11:53:09 -0300

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+  const About = () => {
+  return (
+    <Fragment>
+      <h1>About this app</h1>
+      <p>This app has been built to enable the user to search github profiles</p>
+      <p>Version 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About
